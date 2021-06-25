62935ae8ab345b10d1b4e774f64cfc610f35198e
beataj <beata@emc2wave.com>
2021-06-25 12:02:30 -0700

    add {} to serverelements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{ type: 'server', name: 'test', content: 'Just a server test'}];
   
 
   
